206f99e19fa2a2e769525c96081ba8fadd25f5a8
mosinskimatt <91233770+mosinskimatt@users.noreply.github.com>
2021-10-24 22:19:57 -0400

    Add files via upload

---
+++ Lab4/hello.py
@@ -0,0 +1,13 @@
+# Matthew Mosinski October 21st, 2021
+## Credit to Automate the Boring Stuff with Python
+
+# This program says hello and asks for my name
+print('Hellow world!')
+print('What is your name?') #ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('the length of your name is:')
+print(len(myName))
+print('What is your age?') #ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
